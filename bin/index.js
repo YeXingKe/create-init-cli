0aae7148adcdbea025b8aa5d33261e43add59dd1
zws <ws_zhu@foxmail.com>
2023-06-14 18:16:39 +0800

    feat：add chalk

--- bin/index.js
+++ bin/index.js
@@ -1,18 +1,2 @@
 #!/usr/bin/env node
 
-// const chalk = require('chalk');
-import chalk from 'chalk'
-console.log(chalk.green.bold('successful!'))
-console.log(chalk.green('hello',chalk.underline('world!')))
-
-
-const error = (...text) => console.log(chalk.bold.hex('#ff0000')(text));
-const warning = (...text)=> console.log(chalk.hex('#fffa500')(text));
-const success = (...text) =>console.log(chalk.green(text));
-
-error('Error!')
-warning('Warning!');
-success('Successful!')
-
-// const customChalk = new chalk.Chalk({level:0});
-// console.log(customChalk.blue('hello world!'))
