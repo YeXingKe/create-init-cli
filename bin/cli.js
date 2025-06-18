baa0e0bf17b62f0de471d590d87e12a263fc404c
mason.zhu <ws_zhu@foxmail.com>
2025-06-18 22:15:07 +0800

    feat: add framework termplate

--- bin/cli.js
+++ bin/cli.js
@@ -1,28 +1,31 @@
 #!/usr/bin/env node
 
 // const commander = require('commander');
-const commander = require('commander');
-const pkg = require('../package.json');
-const createProject = require('../lib/create')
+const commander = require("commander");
+const pkg = require("../package.json");
+const createProject = require("../lib/create");
 
 const program = new commander.Command();
+// program
+// .name(Object.keys(pkg.bin)[0]) // 设置程序的名称
+// .usage('<command> [options]') // 提示用户如何使用这个命令行工具
+// .version(pkg.version) // 设置了命令行工具的版本号
+
 program
-.name(Object.keys(pkg.bin)[0]) // 设置程序的名称
-.usage('<command> [options]') // 提示用户如何使用这个命令行工具
-.version(pkg.version) // 设置了命令行工具的版本号
-
-
-program.command('create <app-name>').description('创建新项目').action((projectName) => {
-    createProject(projectName)
-})
+  .command("create <project-name>")
+  .description("创建新项目")
+  .action((projectName) => {
+    console.log(projectName);
+    createProject(projectName);
+  });
 // program.command('install <dependencies...>','install dependencies')
 
 // 处理无命令执行的情况
 if (process.argv.length === 2) {
-    // process.argv是一个数组
-    // ['D:\\develop\\nvm\\nodejs\\node.exe',
-    // 'D:\\develop\\nvm\\node_global\\node_modules\\create-template-cli\\bin\\index.js']
-    program.help();
+  // process.argv是一个数组
+  // ['D:\\develop\\nvm\\nodejs\\node.exe',
+  // 'D:\\develop\\nvm\\node_global\\node_modules\\create-template-cli\\bin\\index.js']
+  program.help();
 }
 
-program.parse(process.argv)
+program.parse(process.argv);
