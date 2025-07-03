8d8209a7c093638888761a0d1f24510c7d84e614
mason.zhu <ws_zhu@foxmail.com>
2025-07-03 15:03:49 +0800

    feat: add template cache

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# create-template-cli
-脚手架工具：可初始化项目，兼容pc端、移动端、uniapp、微信小程序，选择相应ui，默认创建规范文件，调试文件等
+# create-init-cli
+脚手架工具：可初始化项目，通过默认模板生成初始化
 > node >= 18
 ## 命令
 ```json
@@ -7,6 +7,26 @@
   "cptc": "bin/index.js"
 }
 ```
-## 添加命令到全局npm
-在根目录执行
+## 文件
+```json
+bin
+- cli.js                  // 命令启动逻辑
+lib
+- gennerators
+---defaultTemplate        // 从远程git获取的默认配置项目模板逻辑
+---code.json              // 配置默认引入的代码
+---framework.js           // 前端框架引入逻辑
+---index.js
+---lint.js                // 规范插件引入逻辑
+---ui.js                  // ui插件引入逻辑
+- templates               // 模板文件夹
+- create.js               // 项目创建命令交互逻辑
+- saveTemplate.js         // 保存模板配置到本地
+- selectConfig.js         // 交互配置
+- utils.js                // 通用工具
+```
+## 本地调试
+- 在根目录执行
 > npm link
+- 在需要创建项目文件夹下执行
+> npm create app-demo
--- bin/cli.js
+++ bin/cli.js
@@ -17,40 +17,47 @@ const TemplatePreview = require('../lib/generators/defaultTemplate/TemplatePrevi
 // .usage('<command> [options]') // 提示用户如何使用这个命令行工具
 // .version(pkg.version) // 设置了命令行工具的版本号
 
+async function executeFun(options,projectName=null){
+  try {
+    // 初始化模板系统
+    const templateManager = new TemplateManager()
+    const templateSelector = new TemplateSelector(templates)
+    const templatePreview = new TemplatePreview(templateManager)
+    // 步骤1: 选择模板
+    const templateName = await templateSelector.selectTemplate()
+    if (templateName === 'custom') {
+      // 自定义模板
+      createProject(projectName, options)
+    }
+    // // 步骤2: 预览模板
+    // const confirmed = await templatePreview.showPreview(templateName)
+    // if (!confirmed) {
+    //   console.log(chalk.yellow('create project canceled!'))
+    //   process.exit(0)
+    // }
+  } catch (error) {
+    console.error(chalk.red(`\n✖ create project failed: ${error.message}\n`))
+    process.exit(1)
+  }
+}
+
+// 必填项目名
 program
   .command('create <project-name>')
   .description('creat new project')
   .option('--skip-install', 'skip dependences install')
   .action(async (projectName, options) => {
-    try {
-      // 初始化模板系统
-      const templateManager = new TemplateManager()
-      const templateSelector = new TemplateSelector(templates)
-      const templatePreview = new TemplatePreview(templateManager)
-      // 步骤1: 选择模板
-      const templateName = await templateSelector.selectTemplate()
-      if (templateName === 'custom') {
-        // 自定义模板
-        createProject(projectName, options)
-      }
-      // 步骤2: 预览模板
-      const confirmed = await templatePreview.showPreview(templateName)
-      if (!confirmed) {
-        console.log(chalk.yellow('create project canceled!'))
-        process.exit(0)
-      }
-    } catch (error) {
-      console.error(chalk.red(`\n✖ create project failed: ${error.message}\n`))
-      process.exit(1)
-    }
+    await executeFun(options,projectName)
   })
-// program.command('install <dependencies...>','install dependencies')
+// 可选项目名
+program.command('init [project-name]').description('creat new project')
+.option('--skip-install', 'skip dependences install').action(async (projectName,options) => {
+  await executeFun(options,projectName)
+})
 
 // 处理无命令执行的情况
 if (process.argv.length === 2) {
-  // process.argv是一个数组
-  // ['D:\\develop\\nvm\\nodejs\\node.exe',
-  // 'D:\\develop\\nvm\\node_global\\node_modules\\create-template-cli\\bin\\index.js']
+//  ['node路径','脚本命令路径',...args]=process.argv
   program.help()
 }
 
